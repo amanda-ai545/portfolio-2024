4a40aa3e768725d587e7dae6d989c6e22e096b22
Amanda Evallo <amandadianeevallo@gmail.com>
2024-11-12 14:58:17 +0800

    feat: add new projects

---
+++ src/data/projects.tsx
@@ -0,0 +1,21 @@
+import thumbnail1 from '@assets/images/project-1-thumbnail.jpg';
+import thumbnail2 from '@assets/images/project-2-thumbnail.jpg';
+import thumbnail3 from '@assets/images/project-3-thumbnail.jpg';
+import thumbnail4 from '@assets/images/project-4-thumbnail.jpg';
+import thumbnail5 from '@assets/images/project-5-thumbnail.jpg';
+import thumbnail6 from '@assets/images/project-6-thumbnail.jpg';
+import thumbnail7 from '@assets/images/project-7-thumbnail.jpg';
+import thumbnail8 from '@assets/images/project-8-thumbnail.jpg';
+import thumbnail9 from '@assets/images/project-9-thumbnail.jpg';
+
+export const projects = [
+  { title: 'Ads Templates', subtitle: 'Brand content - React JS', imgSrc: thumbnail9 },
+  { title: 'Dog Hotel & Spa', subtitle: 'Website - Wordpress', imgSrc: thumbnail8 },
+  { title: 'Okimeguri', subtitle: 'Website | Admin App - Vue3 JS', imgSrc: thumbnail7 },
+  { title: 'Iriomote Admin', subtitle: 'Admin App - Vue3 JS', imgSrc: thumbnail6 },
+  { title: 'Smart Front Mujinn', subtitle: 'Admin Web App', imgSrc: thumbnail5 },
+  { title: 'Family Docs Box', subtitle: 'Web App - Next JS', imgSrc: thumbnail4 },
+  { title: 'Wacoal Wing', subtitle: 'Website', imgSrc: thumbnail3 },
+  { title: 'Hot Pepper Beauty Academy', subtitle: 'Website', imgSrc: thumbnail2 },
+  { title: 'Project Gueridon', subtitle: 'Website | Application | Smart table', imgSrc: thumbnail1 },
+];
